3720cadb4b01864c909e420b1a08f4fc1c175a28
Mauro Cicerchia <maurocicerchia98@gmail.com>
2021-10-29 12:50:53 -0300

    fixes post reacteca

--- section4-introduccion/src/App.js
+++ section4-introduccion/src/App.js
@@ -3,7 +3,7 @@ import Button from "./Button";
 import Kuberneteca from "./Kuberneteca";
 import "./Main.css";
 
-const Li = ({ children, estado, edad }) => (
+const Pibe = ({ children, estado, edad }) => (
 	<li className="clase-li">
 		Hola, soy {children}, tengo {edad} años y estoy {estado}
 	</li>
@@ -11,28 +11,34 @@ const Li = ({ children, estado, edad }) => (
 
 const chanchitos = ["chanchito feliz 🐷😀", "chanchito triste 🐷😭", "chanchito emocionado 🐷🤩🌚"];
 
+// Como regla general, no usar estilos inline, usar className
+
 function App() {
 	const [showKuberneteca, setShowKuberneteca] = useState(false);
 	return (
 		<div>
 			{showKuberneteca && <Kuberneteca />}
-			<ul className="clase-ul">
-				<Li estado="feliz como Roman" edad={22}>
-					Seba
-				</Li>
-				<Li estado="harto de estudiar" edad={23}>
-					Mauro
-				</Li>
 
-				{chanchitos.map((chanchito, index) => (
-					<p className="clase-chanchito" key={index}>{chanchito}</p>
+			<h1 className="red-text">Hola, soy un componente</h1>
+
+			<ul className="clase-ul">
+				<Pibe estado="feliz como Roman" edad={22}>
+					Seba
+				</Pibe>
+				<Pibe estado="harto de estudiar" edad={23}>
+					Mauro
+				</Pibe>
+
+				{chanchitos.map((chanchito) => (
+					<p className="clase-chanchito" key={chanchito}>{chanchito}</p>
 				))}
 
 				<Button
 					onClick={(e) => {
-						console.log("ctrl pressed", e.ctrlKey);
-						console.log("shift pressed", e.shiftKey);
-						console.log("alt pressed", e.altKey);
+						// console.log(e);
+						// console.log("ctrl pressed", e.ctrlKey);
+						// console.log("shift pressed", e.shiftKey);
+						// console.log("alt pressed", e.altKey);
 						setShowKuberneteca(!showKuberneteca);
 					}}
 				>
--- section4-introduccion/src/index.js
+++ section4-introduccion/src/index.js
@@ -4,40 +4,40 @@ import "./index.css";
 import App from './App';
 import reportWebVitals from "./reportWebVitals";
 
+const x = React.createElement("p", null, "Hola Mauro!");
+
+const X = () => <p>Hola Seba!</p>;
+
+const Pibe = ({ children, estado, edad }) => (
+  <li>
+		Hola, soy {children}, tengo {edad} años y estoy {estado}
+	</li>
+);
+
+const LosPibes = () => (
+  <ul>
+		<Pibe estado="feliz" edad={22}>
+			Seba
+		</Pibe>
+		<Pibe estado="harto de estudiar" edad={23}>
+			Mauro
+		</Pibe>
+	</ul>
+);
+
+// ReactDOM.render(
+//   <div>
+// 		<LosPibes />
+// 	</div>,
+// 	document.getElementById("root2")
+// );
+  
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// const x = React.createElement("p", null, "Hola Mauro!");
-
-// const X = () => <p>Hola Seba!</p>;
-
-// const Li = ({ children, estado, edad }) => (
-// 	<li>
-// 		Hola, soy {children}, tengo {edad} años y estoy {estado}
-// 	</li>
-// );
-
-// const X2 = () => (
-// 	<ul>
-// 		<Li estado="feliz" edad={22}>
-// 			Seba
-// 		</Li>
-// 		<Li estado="harto de estudiar" edad={23}>
-// 			Mauro
-// 		</Li>
-// 	</ul>
-// );
-
-// ReactDOM.render(
-// 	<div>
-// 		<X2 />
-// 	</div>,
-// 	document.getElementById("root")
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
